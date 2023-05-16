5b68b54406da70ba1cc59b8993e032a50c77a605
Juan Chung <juanchung7v@gmail.com>
2023-05-15 23:00:20 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const easyLevelCardNumber = 6;
+const medLevelCardNumber = 12;
+const hardLevelCardNumber = 20;
+
+const firstCardName = "";
+const secondCardName = "";
+
+// Easy Mode
+// Retrive three pokemon images and associate pokemon IDs
+// Populate three cards (twice for each one) using map function
+// Match firstCardName equals secondCardName?
