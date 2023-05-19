8416881a472bb8bbcd39624a8909f5ad120de443
Juan Chung <juanchung7v@gmail.com>
2023-05-19 01:04:16 -0700

    Added Stats and flip functionality

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const easyLevelCardNumber = 6;
 const medLevelCardNumber = 12;
 const hardLevelCardNumber = 20;
+let matchCounter = 0;
+let clickCounter = 0;
+let totalPairs = 0;
+let pairsLeft = 0;
+let selectedLevel = 0;
 
-const firstCardName = "";
-const secondCardName = "";
+let selectedCardArray = [];
+// let matchCardCollection = {};
 
 let pokemons = [];
 let innerResponse = [];
@@ -31,9 +36,48 @@ const start = async () => {
     $('.flip-card-container').empty();
   })
 
+  // play game
+  $('body').on('click', '.flip-card', async function (e) {
+    clickCounter ++;
+    console.log(clickCounter)
+    if ($(this).find('.flip-card-inner').hasClass('matched')) {
+        return;
+    }
 
+    $(this).find('.flip-card-inner').addClass('flipped')
+    let cardId = $(this).find('.pokemonCard').attr('pokemon-id');
+    selectedCardArray.push({ id:cardId, element:this });
+
+    if (selectedCardArray.length == 2) {
+        if (selectedCardArray[0].id === selectedCardArray[1].id) {
+            $(selectedCardArray[0].element).find('.flip-card-inner').addClass('matched');
+            $(selectedCardArray[1].element).find('.flip-card-inner').addClass('matched');
+            console.log("Match!")
+            selectedCardArray = [];
+            matchCounter ++;
+            pairsLeft = totalPairs - matchCounter;
+            console.log(matchCounter);
+        } else {
+            setTimeout(() => {
+                $('.flip-card-inner').not('.matched').removeClass('flipped');
+                selectedCardArray = [];
+            }, 1000);
+        }
+    }
+
+    $('#matches').text(matchCounter)
+    $('#clicks').text(clickCounter)
+    $('#pairs-left').text(pairsLeft)
+  });
 
 }
+
+
+$('.reset-btn').on('click', async function (e) {
+    $('.flip-card-container').empty();
+    $('.flip-card-inner').removeClass('matched');
+    $('.stat').empty();
+})
 
 // random number generator
 function getRandomInt(max = 100) {
@@ -44,14 +88,19 @@ function getRandomInt(max = 100) {
 async function displayCards(pokemons, level) {
 
     // select number of cards based on level
-    let selectedLevel = 0;
+
     if (level == 1) {
         selectedLevel = easyLevelCardNumber;
+        totalPairs = easyLevelCardNumber / 2;
     } else if (level == 2) {
         selectedLevel = medLevelCardNumber;
+        totalPairs = medLevelCardNumber / 2;
     } else if (level == 3) {
         selectedLevel = hardLevelCardNumber;
+        totalPairs = hardLevelCardNumber / 2;
     };
+
+    $('#total-pairs').text(totalPairs);
 
     $('.flip-card-back').empty();
     let cards = [];
@@ -69,7 +118,7 @@ async function displayCards(pokemons, level) {
                             <img src="./public/pokeball.png" alt="Poketball" style="width: 100px; height: 100px"/>
                         </div>
                         <div class="flip-card-back">
-                            <div class="PokemonCard" pokemon-id=${thisPokemon.id} pokemon-name=${thisPokemon.name}>
+                            <div class="pokemonCard" pokemon-id=${thisPokemon.id} pokemon-name=${thisPokemon.name}>
                             <img src="${thisPokemon.sprites.front_default}" alt="${thisPokemon.name}" style="width: 200px; height: 200px"/>
                             <h3 style="color: black">${thisPokemon.name}</h3>
                             </div>
